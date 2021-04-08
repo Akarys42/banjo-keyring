7d0a31b2f8022c6db8123824a747083456479e50
Matteo Bertucci <matteobertucci2004@gmail.com>
2021-04-08 16:17:55 +0200

    Logging boilerplate

---
+++ src/logging.rs
@@ -0,0 +1,16 @@
+use simplelog::{TermLogger, LevelFilter, ConfigBuilder, TerminalMode, ColorChoice};
+use log::SetLoggerError;
+
+/// Initialize the logger for the CLI app
+pub fn init_cli_logging(level: LevelFilter) -> Result<(), SetLoggerError> {
+    TermLogger::init(
+        level,
+        ConfigBuilder::new()
+            .set_time_level(LevelFilter::Off)
+            .set_location_level(LevelFilter::Off)
+            .set_target_level(LevelFilter::Off)
+            .build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod logging;
+
 #[macro_use]
 extern crate clap;
 
 use clap::{App, SubCommand};
+use crate::logging::init_cli_logging;
+use simplelog::LevelFilter;
+use log::warn;
 
 fn main() {
     let cli_yaml = load_yaml!("cli-definition.yaml");
@@ -13,6 +18,11 @@ fn main() {
     let debug_cli_yaml = load_yaml!("cli-debug-subcommand.yaml");
     #[cfg(feature = "enable_debug")]
     let app = app.subcommand(SubCommand::from_yaml(debug_cli_yaml));
+
+    init_cli_logging(LevelFilter::Warn).expect("Failed to initialize logging.");
+
+    #[cfg(feature = "enable_debug")]
+    warn!("Debug mode is enabled! NOT SUITABLE FOR PRODUCTION.");
 
     app.get_matches();
 }
