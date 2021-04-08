db748feb67593a8b150d02efc08665c3ddfe5330
Matteo Bertucci <matteobertucci2004@gmail.com>
2021-04-08 15:45:03 +0200

    Clap boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+#[macro_use]
+extern crate clap;
 
+use clap::{App, SubCommand};
+
+fn main() {
+    let cli_yaml = load_yaml!("cli-definition.yaml");
+    let app = App::from_yaml(cli_yaml);
+
+
+    // Add debug command if required.
+    #[cfg(feature = "enable_debug")]
+    let debug_cli_yaml = load_yaml!("cli-debug-subcommand.yaml");
+    #[cfg(feature = "enable_debug")]
+    let app = app.subcommand(SubCommand::from_yaml(debug_cli_yaml));
+
+    app.get_matches();
 }
